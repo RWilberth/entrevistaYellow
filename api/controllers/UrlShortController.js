7c2edc4b44117dd7ed2f55bc37e22e8439da1745
wilberthRivasB <wilberth.rivas@ensitech.com>
2018-07-17 23:35:03 -0500

    Elaboracion de pruebas automatizadas

--- api/controllers/UrlShortController.js
+++ api/controllers/UrlShortController.js
@@ -9,75 +9,93 @@ var emptyValidation = require('../../validations/EmptyValidator.js');
 var urlValidation = require('../../validations/UrlValidator.js');
 var hashGenerator = require('../../components/hashGenerator.js');
 module.exports = {
-  validateUrls: function(array){
-  	var result =  !array.some(function(element){ 
-  		var validation = emptyValidation.isValid(element) && urlValidation.isValid(element);
-  		return !validation;
-  	});
-  	return result;
-  },
-  redirectUrl: function(req, res){
-    var params = req.params;
-    var hashToken = params.hashToken;
-    if(!emptyValidation.isValid(hashToken)){
-      return res.json({message: "Token value is required for this endpoint"});
-    }
-    var hashNumber = hashGenerator.convertBase62ToBase10(params.hashToken);
-    console.log(hashNumber);
+	validateUrls: function (array) {
+		var result = !array.some(function (element) {
+				var validation = emptyValidation.isValid(element) && urlValidation.isValid(element);
+				return !validation;
+			});
+		return result;
+	},
+	redirectUrl: function (req, res) {
+		var params = req.params;
+		var hashToken = params.hashToken;
+		if (!emptyValidation.isValid(hashToken)) {
+			return res.json({
+				message: "Token value is required for this endpoint"
+			});
+		}
+		var hashNumber = hashGenerator.convertBase62ToBase10(params.hashToken);
+		
+		UrlShort.findOne({
+			hash: hashNumber
+		}).then(function (urlShort) {
+			if (!urlShort) {
+				return res.notFound('Could not your url, sorry.');
+			} else {
+				return res.redirect(urlShort.url);
+			}
+		}).catch(function (e) {
+			console.log(e);
+			return res.json({
+				message: e
+			});
+		})
+	},
+	bulk: function (req, res) {
+		//Validate if the body is not empty
+		if (!emptyValidation.isValid(req.body)) {
+			return res.json({
+				message: "Json is required for this endpoint"
+			});
+		}
+		//validate that the body is an array
+		if (!Array.isArray(req.body)) {
+			return res.json({
+				message: "Json should be an array"
+			});
+		}
+		//Validate that each element has the url format
+		if (!module.exports.validateUrls(req.body)) {
+			return res.json({
+				message: "Some element in the array is empty or not have the url format"
+			});
+		}
+		Counter.findOrCreate({
+			code: 'UrlShortSequence'
+		}, {
+			code: 'UrlShortSequence',
+			sequence: 0
+		})
+		.exec(function (err, counter, wasCreated) {
+			if (err) {
+				console.log(err);
+				return res.json({
+					message: err
+				});
+			}
+			var urls = req.body.map(function (url) {
+					return {
+						url: url
+					};
+				});
+			UrlShort.createEach(urls).fetch().then(function (urlsCreated) {
+				var hashCreated = urlsCreated.map(function (url) {
+						var numToHash = url.hash.toString();
+						var hash = hashGenerator.convertBase10ToBase62(parseInt(numToHash));
+						return {
+							hash: hash,
+							url: url.url
+						};
+					});
+				return res.json(hashCreated);
+			}).catch(function (err) {
+				console.log(err);
+				return res.json({
+					message: err
+				});
+			});
 
-    UrlShort.find({hash: hashNumber}).then(function(urls){
-      console.log(urls);
-    });
-    UrlShort.findOne({hash: hashNumber}).then(function(urlShort){
-      console.log(urlShort);
-      if(!urlShort){
-        return res.notFound('Could not your url, sorry.');
-      }else{
-        return res.redirect(urlShort.url);
-      }
-    }).catch(function(e){
-      console.log(e);
-      return res.json({message: e});
-    })
-  },
-  bulk: function(req, res){
-
-   
-  	//Validate if the body is not empty
-  	if(!emptyValidation.isValid(req.body)){
-  		return res.json({message: "Json is required for this endpoint"});
-  	}
-  	//validate that the body is an array
-  	if(!Array.isArray(req.body)){
-  		return res.json({message: "Json should be an array"});
-  	}
-  	//Validate that each element has the url format
-  	if(!module.exports.validateUrls(req.body)){
-  		return res.json({message: "Some element in the array is empty or not have the url format"});
-  	}
-     Counter.findOrCreate({ code: 'UrlShortSequence' }, { code: 'UrlShortSequence', sequence: 0 })
-        .exec(function(err, counter, wasCreated){
-            if (err) { 
-                console.log(err);
-                return res.json({message: err});
-            }
-            var urls = req.body.map(function(url){
-              return {url:url};
-            });
-          UrlShort.createEach(urls).fetch().then(function(urlsCreated){
-            var hashCreated = urlsCreated.map(function(url){
-              var numToHash = url.hash.toString();
-              var hash = hashGenerator.convertBase10ToBase62(parseInt(numToHash));
-              return {hash: hash, url: url.url};
-            });
-            return res.json(hashCreated);
-        }).catch(function(err){
-          console.log(err);
-          return res.json({message: err});
-        });
-  	
-	 });
-  }
+		});
+	}
 
 };
-
---
+++ test/integration/controllers/UrlShortController.test.js
@@ -0,0 +1,118 @@
+var supertest = require('supertest');
+var assert = require('chai').assert;
+var UrlShort = require('../../../api/models/UrlShort.js');
+
+describe('UrlShortController.bulk', function () {
+	describe('POST /url/bulk', function () {
+		it('responds with json', function (done) {
+			supertest(sails.hooks.http.app)
+			.post('/url/bulk')
+			.set('Accept', 'application/json')
+			.send([])
+			.expect('Content-Type', /json/)
+			.expect({ message: 'Json is required for this endpoint' })
+			.expect(200, done);
+		});
+	});
+	var urls = ['https://github.com/visionmedia/supertest', 'https://sailsjs.com/documentation/concepts/testing'];
+	describe('POST /url/bulk', function () {
+		it('responds with json', function (done) {
+		 	supertest(sails.hooks.http.app)
+			.post('/url/bulk')
+			.set('Accept', 'application/json')
+			.send(urls)
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.then(res=>{
+				assert.isArray(res.body);
+				assert.lengthOf(res.body, 2);
+				assert.isTrue(res.body.every((element)=>{
+					return element.hasOwnProperty('hash') && element.hasOwnProperty('url');
+				}), "Each element espected hash and url.");
+				done();
+			}).catch(function(e){
+				done(e);
+			});
+		});
+	});
+	var failUrls = ['wqeqweqwe','https://sailsjs.com/documentation/concepts/testing'];
+	describe('POST /url/bulk', function () {
+		it('responds with json', function (done) {
+			supertest(sails.hooks.http.app)
+			.post('/url/bulk')
+			.set('Accept', 'application/json')
+			.send(failUrls)
+			.expect('Content-Type', /json/)
+			.expect({
+				message:"Some element in the array is empty or not have the url format"
+			})
+			.expect(200, done);
+		});
+	});
+	var massiveUrls = [];
+	var amountElements = 5000;
+	for(var i = 0; i < amountElements; i++){
+		massiveUrls.push('https://sailsjs.com/documentation/concepts/testing');
+	}
+	describe('POST /url/bulk', function () {
+		it('responds with json', function (done) {
+			supertest(sails.hooks.http.app)
+			.post('/url/bulk')
+			.set('Accept', 'application/json')
+			.send(massiveUrls)
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.then(res=>{
+				assert.isArray(res.body);
+				assert.lengthOf(res.body, amountElements);
+				assert.isTrue(res.body.every((element)=>{
+					return element.hasOwnProperty('hash') && element.hasOwnProperty('url');
+				}), "Each element espected hash and url.");
+				done();
+			}).catch(function(e){
+				done(e);
+			});
+		});
+	});
+});
+describe('UrlShortController.redirectUrl', function () {
+	var urlsToRedirect = ['https://www.google.com'];
+	
+	describe('Get /', function () {
+		it('responds with redirect', function (done) {
+			supertest(sails.hooks.http.app)
+			.post('/url/bulk')
+			.set('Accept', 'application/json')
+			.send(urlsToRedirect)
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.then((urlsToGet)=>{
+				assert.isArray(urlsToGet.body);
+				assert.lengthOf(urlsToGet.body, 1);
+				assert.isTrue(urlsToGet.body.every((element)=>{
+					return element.hasOwnProperty('hash') && element.hasOwnProperty('url');
+				}), "Each element espected hash and url.");
+				var firstUrl = urlsToGet.body[0];
+				return supertest(sails.hooks.http.app)
+				.get('/'+firstUrl.hash)
+				.expect(302)
+				.expect('Location', firstUrl.url)
+				.then(function(res){
+					done();
+				}).catch((e)=>{
+					done(e);
+				});
+			}).catch((e)=>{
+				done(e);
+			});
+			
+		});
+	});
+	describe('Get /', function () {
+		it('responds with Not Found', function (done) {
+			supertest(sails.hooks.http.app)
+			.get('/123')
+			.expect(404, done);
+		});
+	});
+});
